d255a6ef71fde1bf718cf9f51d1fe6d87b4679b6
ZhuGary878 <ZhuGary878@2040I-L148-04.tvnet.Pri>
2021-11-04 13:48:01 -0400

    .

--- src/com/teamworkmakesthedreamwork/Effects.java
+++ src/com/teamworkmakesthedreamwork/Effects.java
@@ -1,5 +1,38 @@
 package com.teamworkmakesthedreamwork;
 
 public class Effects {
+	private int x, y; 
+	private Boolean status;
+	
+	public Effects(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.status = false; 
+	}
 
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+	
+	
 }
